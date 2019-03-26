68dfe275f78b99205bb1b0b47326ce8046347879
ren <x2615939@qq.com>
2019-03-26 21:34:53 +0800

    fix a little bug

--- test_2_ren.py
+++ test_2_ren.py
@@ -1,24 +1,17 @@
 # coding=utf-8
 
 
-import sys
-
-
-def expr(args):
-    calc_list = list(set(args) & {"+", "-", "*", "%"})
-    num_list = list(set(args) - set(calc_list))
-    calc_express = ""
-    for _ in range(len(calc_list)):
-        num = num_list.pop(0)
-        calc_express += num
-        calc = calc_list.pop(0)
-        calc_express += calc
-    calc_express += num_list[-1]
-    print(calc_express)
-    print(eval(calc_express[::-1]))
+def cal(s):
+    nums = []
+    l = ['+', '-', '*', '/']
+    for i in s[5:].split():
+        if i not in l:
+            nums.append(i)
+        else:
+            a, b = nums.pop(), nums.pop()
+            nums.append(str(eval(b + i + a)))
+    return eval(nums.pop())
 
 
 if __name__ == '__main__':
-    str_expr = input()
-    args = str_expr.split(" ")[1:]
-    expr(args)
+    print(cal('expr 2 3 4 + *'))
