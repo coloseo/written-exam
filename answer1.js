8cc147c3a013368e8d12058940931329d5e94088
AurorePaladin <aurorepaladin@gmail.com>
2019-03-29 14:06:41 +0800

    王忆碚修改第一题

--- answer1.js
+++ answer1.js
@@ -1,11 +1,20 @@
 
 // 1. 编写一个递归版本的 reverse(s) 函数(或方法),以将字符串s倒置。
 
+// function reverse(str) {
+// 	var myArray=str.split("");
+//   	var a = myArray.reverse();
+//  	var str1 = a.join("");
+//   	return str1;
+// }
+
+// 修改：
+
 function reverse(str) {
-	var myArray=str.split("");
-  	var a = myArray.reverse();
- 	var str1 = a.join("");
-  	return str1;
+	if ((null == str) || (str.length <= 1)) {
+		return str;
+	}
+	return reverse(str.substring(1)) + str.charAt(0);
 }
 
 var s = "WangYiBei!"
